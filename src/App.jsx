2f3d507252b45399f2e4bbf2e796918dca24bd0f
Matheus <Matheus.feques99@gmail.com>
2025-05-26 20:40:11 -0300

    atual

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Nada ainda</h1>
+      <h1>nada</h1>
     </>
   )
 }
