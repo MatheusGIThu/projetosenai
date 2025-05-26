cd42e1ea6460cbfd890b137ef1f2c9895d35222b
Matheus <Matheus.feques99@gmail.com>
2025-05-26 19:32:47 -0300

    iniciou

---
+++ src/cadastro.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+
+function cadastro (){
+    return(
+        <>
+        <div className='form'>
+            <form action=""></form>
+        </div>
+        </>
+    )
+}
